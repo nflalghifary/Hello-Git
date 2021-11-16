91ebc27f043a575094863f9dcc55946ae684de9d
yogimahaputra <yogi.mahaputra123@gmail.com>
2021-11-16 10:33:53 +0700

    GET API Provinsi

--- app/src/main/java/com/example/hellogit/Adapter.java
+++ app/src/main/java/com/example/hellogit/Adapter.java
@@ -2,6 +2,7 @@ package com.example.hellogit;
 
 import android.content.Context;
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +11,30 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
 
     private LayoutInflater layoutInflater;
     private List<String> data;
+    private List<String> data2;
+    private List<String> data3;
+    private List<String> data4;
+    private List<String> data5;
+    private List<String> data6;
+    private List<String> data7;
 
-    Adapter(Context context,List<String> data){
+    Adapter(Context context, List<String> data, ArrayList<String> data2, ArrayList<String> data3, ArrayList<String> data4, ArrayList<String> data5, ArrayList<String> data6, ArrayList<String> data7 ){
          this.layoutInflater = LayoutInflater.from(context);
          this.data = data;
+         this.data2 = data2;
+         this.data3 = data3;
+         this.data4 = data4;
+         this.data5 = data5;
+         this.data6 = data6;
+         this.data7 = data7;
     }
 
     @NonNull
@@ -29,11 +44,35 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
         return new ViewHolder(view);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public Object convertDecimal(String a){
+        String number = a;
+        double amount = Double.parseDouble(number);
+        DecimalFormat formatter = new DecimalFormat("#,###");
+// the zeroes after the point are the number of digits shown after the period
+// you can also switch point and commas and get for example 1.002,45
+        String res = String.valueOf(formatter.format(amount));
 
-        String namaProvinsi = data.get(position);
-        holder.namaProvinsi.setText(namaProvinsi);
+        return res;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder,  int position) {
+//        Log.e("ini bind", String.format("%,d", data2.get(position) ));
+        String nameProvinsi = data.get(position);
+        String totalKasus = (String) convertDecimal(data2.get(position));
+        String sembuh = (String) convertDecimal(data3.get(position));
+        String meninggal = (String) convertDecimal(data4.get(position));
+        String positif = (String) convertDecimal(data5.get(position));
+        String penambahanKasus = (String) convertDecimal(data6.get(position));
+        String penambahanSembuh = (String) convertDecimal(data7.get(position));
+
+        holder.namaProvinsi.setText(nameProvinsi);
+        holder.totalKasus.setText(totalKasus);
+        holder.sembuh.setText(sembuh);
+        holder.meninggal.setText(meninggal);
+        holder.positif.setText(positif);
+        holder.penambahanKasus.setText(penambahanKasus);
+        holder.penambahanSembuh.setText(penambahanSembuh);
     }
 
     @Override
--- app/src/main/java/com/example/hellogit/ProvinsiActivity.java
+++ app/src/main/java/com/example/hellogit/ProvinsiActivity.java
@@ -5,29 +5,165 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ProvinsiActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     Adapter adapter;
     ArrayList<String> namaProvinsi;
+    ArrayList<String> totalKasus;
+    ArrayList<String> sembuh;
+    ArrayList<String> meninggal;
+    ArrayList<String> positif;
+    ArrayList<String> penambahanKasus;
+    ArrayList<String> penambahanSembuh;
+
+    String[] tmp =  new String[10];
+    String URL = "https://data.covid19.go.id/public/api/prov.json";
+    String[] jsonResponses = new String[10];
+    String[] key;
+    String[] jumlah_kasus;
+    String[] jumlah_sembuh;
+    String[] jumlah_meninggal;
+    String[] jumlah_dirawat;
+    String[] penambahanpositif;
+    String[] penambahannegatif;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provinsi);
-        String[] provinsi = getResources().getStringArray(R.array.Provinsi);
-        namaProvinsi = new ArrayList<>();
-        for(int i = 0; i < provinsi.length; i++){
-            namaProvinsi.add(String.valueOf(provinsi[i]));
-        }
+//        String[] provinsi =  new String[10];
+//
+//        namaProvinsi = new ArrayList<>();
+//        for(int i = 0; i < jsonResponses.length; i++){
+//            namaProvinsi.add(String.valueOf(jsonResponses[i]));
+//        }
+//
+//        recyclerView = findViewById(R.id.recyclerProvinsi);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        adapter = new Adapter(this, namaProvinsi);
+//        recyclerView.setAdapter(adapter);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        recyclerView = findViewById(R.id.recyclerProvinsi);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new Adapter(this, namaProvinsi);
-        recyclerView.setAdapter(adapter);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        String[] tes = {"tes","tes2"};
+        Log.e("hasil provinsi", Arrays.toString(tes));
+
+        String url = "https://data.covid19.go.id/public/api/prov.json";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("list_data");
+                    key = new String[jsonArray.length()];;
+                    jumlah_kasus = new String[jsonArray.length()];;
+                    jumlah_sembuh = new String[jsonArray.length()];;
+                    jumlah_meninggal = new String[jsonArray.length()];;
+                    jumlah_dirawat = new String[jsonArray.length()];;
+                    penambahanpositif = new String[jsonArray.length()];;
+                    penambahannegatif = new String[jsonArray.length()];;
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        key[i] = jsonObject.getString("key");
+                        jumlah_kasus[i] = jsonObject.getString("jumlah_kasus");
+                        jumlah_sembuh[i] = jsonObject.getString("jumlah_sembuh");
+                        jumlah_meninggal[i] = jsonObject.getString("jumlah_meninggal");
+                        jumlah_dirawat[i] = jsonObject.getString("jumlah_dirawat");
+
+                        JSONObject penambahan = jsonObject.getJSONObject("penambahan");
+                        Log.e("penambahan object", String.valueOf(penambahan));
+
+                        penambahanpositif[i] = penambahan.getString("positif");
+                        penambahannegatif[i] = penambahan.getString("sembuh");
+
+                        Log.e("penambahan positif", String.valueOf(penambahanpositif));
+                        Log.e("penambahan negatif", String.valueOf(penambahannegatif));
+
+                    }
+
+                    namaProvinsi = new ArrayList<>();
+                    totalKasus = new ArrayList<>();
+                    sembuh = new ArrayList<>();
+                    meninggal = new ArrayList<>();
+                    positif = new ArrayList<>();
+                    penambahanKasus = new ArrayList<>();
+                    penambahanSembuh = new ArrayList<>();
+
+                    for(int i = 0; i < key.length; i++){
+                        namaProvinsi.add(String.valueOf(key[i]));
+                    }
+
+                    for(int i = 0; i < jumlah_kasus.length; i++){
+                        totalKasus.add(String.valueOf(jumlah_kasus[i]));
+                    }
+                    for(int i = 0; i < jumlah_sembuh.length; i++){
+                        sembuh.add(String.valueOf(jumlah_sembuh[i]));
+                    }
+                    for(int i = 0; i < jumlah_meninggal.length; i++){
+                        meninggal.add(String.valueOf(jumlah_meninggal[i]));
+                    }
+                    for(int i = 0; i < jumlah_dirawat.length; i++){
+                        positif.add(String.valueOf(jumlah_dirawat[i]));
+                    }
+                    for(int i = 0; i < penambahanpositif.length; i++){
+                        penambahanKasus.add(String.valueOf(penambahanpositif[i]));
+                    }
+                    for(int i = 0; i < penambahannegatif.length; i++){
+                        penambahanSembuh.add(String.valueOf(penambahannegatif[i]));
+                    }
+
+                    recyclerView = findViewById(R.id.recyclerProvinsi);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(ProvinsiActivity.this));
+
+//                    ArrayList<String>[] mapping = new ArrayList[]{namaProvinsi, totalKasus};
+
+                    adapter = new Adapter(ProvinsiActivity.this, namaProvinsi, totalKasus, sembuh, meninggal, positif, penambahanKasus, penambahanSembuh);
+
+//                    adapter = new Adapter(ProvinsiActivity.this, totalKasus);
+                    recyclerView.setAdapter(adapter);
+                    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                    Log.e("hasil provinsi", Arrays.toString(key));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+
+
+
+
     }
+
+
+
+
 }
